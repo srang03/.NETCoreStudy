71ba9ff810bedc8230e0d61ee51fc17101db96c0
unknown <srang9503@gmail.com>
2022-10-10 22:18:57 +0900

    221010

---
+++ MVCBookWeb/Migrations/20221010131054_AddPeopleToDatabase.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MVCBookWeb.Migrations
+{
+    public partial class AddPeopleToDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ MVCBookWeb/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVCBookWeb.Models
+{
+    public class Category
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+
+        public DateTime CreatedTime { get; set; } = DateTime.Now;
+    }
+}
